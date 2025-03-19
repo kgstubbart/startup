c92f34e509941e1d54f57e305e5a04443da61015
Kason Stubbart <kgstubbart@gmail.com>
2025-03-19 14:14:31 -0600

    jsx for each folder

---
+++ startupReactP1/src/myace/myace.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function MyAce() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>myace displayed here</div>
+    </main>
+  );
+}
---
+++ startupReactP1/src/rankings/rankings.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Rankings() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>rankings displayed here</div>
+    </main>
+  );
+}
