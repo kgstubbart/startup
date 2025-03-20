ad693288b6b43d5c0ca2cb476f31b08d48342a94
Kason Stubbart <kgstubbart@gmail.com>
2025-03-19 21:51:47 -0600

    moved all html pages content to jsx

--- startupReactP1/src/login/login.jsx
+++ startupReactP1/src/login/login.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
+    <main className="container-fluid bg-book-paper text-center">
+            <div>
+                <h5>Welcome to Library Ace! Login to start:</h5>
+                <form method="get" action="myace.html">
+                    <div className="input-group mb-3">
+                    <span className="input-group-text">👤</span>
+                    <input className="form-control" type="text" placeholder="username" />
+                    </div>
+                    <div className="input-group mb-3">
+                    <span className="input-group-text">🔒</span>
+                    <input className="form-control" type="password" placeholder="password" />
+                    </div>
+                    <p></p>
+                    <button type="submit" className="btn btn-secondary">Sign Up</button>
+                    <button type="submit" className="btn btn-primary">Login</button>
+                </form>
+            </div>
+        </main>
   );
 }
--- startupReactP1/src/rankings/rankings.jsx
+++ startupReactP1/src/rankings/rankings.jsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import './rankings.css';
 
 export function Rankings() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>rankings displayed here</div>
-    </main>
+    <main className="container-fluid bg-book-paper text-center">
+			<table className="table table-warning table-striped top-aces-table">
+				<caption className="fs-4 fw-bold">Top Aces</caption>
+				<thead className="table-dark">
+				  <tr>
+					<th>Rank</th>
+					<th>Aces</th>
+					<th>Title</th>
+					<th>Author</th>
+				  </tr>
+				</thead>
+				<tbody>
+				  <tr>
+					<td>#1</td>
+					<td>23</td>
+					<td>Mistborn: The Final Empire</td>
+					<td>Brandon Sanderson</td>
+				  </tr>
+				  <tr>
+					<td>#2</td>
+					<td>19</td>
+					<td>Harry Potter</td>
+					<td>JK Rowling</td>
+				  </tr>
+				  <tr>
+					<td>#3</td>
+					<td>18</td>
+					<td>Eragon</td>
+					<td>Christopher Paolini</td>
+				  </tr>
+				</tbody>
+			  </table>
+
+			  <table className="table table-warning table-striped recent-aces-table">
+				<caption className="fs-4 fw-bold">Recent Aces</caption>
+				<thead className="table-dark">
+				  <tr>
+					<th>User</th>
+					<th>Title</th>
+				  </tr>
+				</thead>
+				<tbody>
+				  <tr>
+					<td>Elend</td>
+					<td>Mistborn: The Final Empire</td>
+				  </tr>
+				  <tr>
+					<td>Kaladin</td>
+					<td>Stormlight Archive</td>
+				  </tr>
+				  <tr>
+					<td>Nephi</td>
+					<td>The Book of Mormon</td>
+				  </tr>
+				</tbody>
+			  </table>
+        </main>
   );
 }
