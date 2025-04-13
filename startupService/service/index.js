b13f5e73c4e2e3fca0eb6de16684c586d83a101b
Kason Stubbart <kgstubbart@gmail.com>
2025-04-12 20:55:39 -0600

    login endpoint

--- startupService/service/index.js
+++ startupService/service/index.js
@@ -22,13 +22,28 @@ app.use('/api', apiRouter);
 
 // === Auth Endpoints ===
 
+// CreateAuth a new user
 apiRouter.post('/auth/create', async (req, res) => {
     if (await findUser('username', req.body.username)) {
-      res.status(409).send({ msg: 'Existing user' });
+        res.status(409).send({ msg: 'Existing user' });
     } else {
-      const user = await createUser(req.body.username, req.body.password);
-  
+        const user = await createUser(req.body.username, req.body.password);
+
+        setAuthCookie(res, user.token);
+        res.send({ username: user.username });
+    }
+});
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('username', req.body.username);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
       setAuthCookie(res, user.token);
       res.send({ username: user.username });
+      return;
     }
-  });
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
