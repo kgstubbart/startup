457eddfc61e879bb38adb312d114251a8d785bb6
Kason Stubbart <kgstubbart@gmail.com>
2025-04-16 00:44:30 -0600

    brought in GameEventNotifier from Simon

--- startupWebsocket/src/rankings/rankingsNotifier.js
+++ startupWebsocket/src/rankings/rankingsNotifier.js
@@ -6,11 +6,57 @@ const RankingEvent = {
 
 class EventMessage {
     constructor(user, type, book) {
-      this.from = user;
-      this.type = type;
-      this.value = { title: book };
+        this.from = user;
+        this.type = type;
+        this.value = { title: book };
     }
-  }
+}
+
+class GameEventNotifier {
+    events = [];
+    handlers = [];
+
+    constructor() {
+        let port = window.location.port;
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+        this.socket.onopen = (event) => {
+            this.receiveEvent(new EventMessage('Simon', GameEvent.System, { msg: 'connected' }));
+        };
+        this.socket.onclose = (event) => {
+            this.receiveEvent(new EventMessage('Simon', GameEvent.System, { msg: 'disconnected' }));
+        };
+        this.socket.onmessage = async (msg) => {
+            try {
+                const event = JSON.parse(await msg.data.text());
+                this.receiveEvent(event);
+            } catch {}
+        };
+    }
+
+    broadcastEvent(from, type, value) {
+        const event = new EventMessage(from, type, value);
+        this.socket.send(JSON.stringify(event));
+    }
+
+    addHandler(handler) {
+        this.handlers.push(handler);
+    }
+
+    removeHandler(handler) {
+        this.handlers.filter((h) => h !== handler);
+    }
+
+    receiveEvent(event) {
+        this.events.push(event);
+
+        this.events.forEach((e) => {
+            this.handlers.forEach((handler) => {
+                handler(e);
+            });
+        });
+    }
+}
 
 function broadcastEvent(user, eventType, book) {
     for (const callback of users) {
