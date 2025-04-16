6ce49daf69a3c2e43df70a06dc3a30f8bc90418e
Kason Stubbart <kgstubbart@gmail.com>
2025-04-16 00:47:19 -0600

    removed onopen and onclose since I am not using those

--- startupWebsocket/src/rankings/rankingsNotifier.js
+++ startupWebsocket/src/rankings/rankingsNotifier.js
@@ -12,20 +12,14 @@ class EventMessage {
     }
 }
 
-class GameEventNotifier {
-    events = [];
+class RankingEventNotifier {
     handlers = [];
 
     constructor() {
         let port = window.location.port;
         const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
         this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
-        this.socket.onopen = (event) => {
-            this.receiveEvent(new EventMessage('Simon', GameEvent.System, { msg: 'connected' }));
-        };
-        this.socket.onclose = (event) => {
-            this.receiveEvent(new EventMessage('Simon', GameEvent.System, { msg: 'disconnected' }));
-        };
+        
         this.socket.onmessage = async (msg) => {
             try {
                 const event = JSON.parse(await msg.data.text());
