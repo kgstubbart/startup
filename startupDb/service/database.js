895ee05b66e7c219b78f89c209cb2bcfe2f0d36f
Kason Stubbart <kgstubbart@gmail.com>
2025-04-15 20:05:48 -0600

    getUser

--- startupDb/service/database.js
+++ startupDb/service/database.js
@@ -17,3 +17,46 @@ const scoreCollection = db.collection('aces');
     process.exit(1);
   }
 })();
+
+function getUser(username) {
+  return userCollection.findOne({ username });
+}
+
+// function getUser(email) {
+//   return userCollection.findOne({ email: email });
+// }
+
+// function getUserByToken(token) {
+//   return userCollection.findOne({ token: token });
+// }
+
+// async function addUser(user) {
+//   await userCollection.insertOne(user);
+// }
+
+// async function updateUser(user) {
+//   await userCollection.updateOne({ email: user.email }, { $set: user });
+// }
+
+// async function addScore(score) {
+//   return scoreCollection.insertOne(score);
+// }
+
+// function getHighScores() {
+//   const query = { score: { $gt: 0, $lt: 900 } };
+//   const options = {
+//     sort: { score: -1 },
+//     limit: 10,
+//   };
+//   const cursor = scoreCollection.find(query, options);
+//   return cursor.toArray();
+// }
+
+// module.exports = {
+//   getUser,
+//   getUserByToken,
+//   addUser,
+//   updateUser,
+//   addScore,
+//   getHighScores,
+// };
