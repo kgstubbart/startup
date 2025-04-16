b4a485c6c072bf8e2d6a1fcf571555ad9256f30a
Kason Stubbart <kgstubbart@gmail.com>
2025-04-16 16:43:53 -0600

    websocket is working when deployed now, needed to install ws

--- startupWebsocket/src/rankings/rankingsNotifier.js
+++ startupWebsocket/src/rankings/rankingsNotifier.js
@@ -16,9 +16,8 @@ class RankingEventNotifier {
     handlers = [];
 
     constructor() {
-        let port = window.location.port;
         const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
-        this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+        this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
         
         this.socket.onmessage = async (msg) => {
             try {
